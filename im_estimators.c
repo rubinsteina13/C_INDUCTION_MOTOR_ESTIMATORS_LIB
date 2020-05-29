f53beeaa04322eda1c886e454babbbd91cc7befd
Serhii Yatsenko <royalroad1995@gmail.com>
2020-05-29 12:20:19 +0300

    Update im_estimators.c

--- im_estimators.c
+++ im_estimators.c
@@ -5,10 +5,10 @@
   * @version V1.0
   * @date    May-2020
   * @brief   This file provides firmware function for implementation the following
-  *			 		 types induction motor estimators:
-  *						+	sensored rotor flux (angle, magnitude) and back-EMF observer; 
-  *						+	sensorless stator back-EMF observer; 
-  *						+	sensorless rotor speed and flux (angle, magnitude) observer.
+  *	     types induction motor estimators:
+  *		+ sensored rotor flux (angle, magnitude) and back-EMF observer; 
+  *		+ sensorless stator back-EMF observer; 
+  *		+ sensorless rotor speed and flux (angle, magnitude) observer.
   ***********************************************************************************
   * @license
   *
@@ -55,14 +55,14 @@ void tIMparams_init(tIMparams* ptIMparams)
 {
 	ptIMparams->f1divTr = ptIMparams->fRr/ptIMparams->fLr;
 	ptIMparams->f1divKr = ptIMparams->fLr/ptIMparams->fLm;
-	ptIMparams->fSigLs	= (1.0f - ptIMparams->fLm*ptIMparams->fLm/
-											(ptIMparams->fLs*ptIMparams->fLr)) * ptIMparams->fLs;
+	ptIMparams->fSigLs = (1.0f - ptIMparams->fLm*ptIMparams->fLm/
+			     (ptIMparams->fLs*ptIMparams->fLr)) * ptIMparams->fLs;
 }
 
 /**
   * @brief  IM stator back-EMF observer calculation function
   * @param  ptIMstatObs: pointer to user data structure with type "tIMstatObs",
-  *			ptIMparams: pointer to user data structure with type "tIMparams".               
+  *	    ptIMparams: pointer to user data structure with type "tIMparams".               
   * @retval None
   */
 void tIMstatObs_calc(tIMstatObs* ptIMstatObs, tIMparams* ptIMparams)
@@ -70,39 +70,39 @@ void tIMstatObs_calc(tIMstatObs* ptIMstatObs, tIMparams* ptIMparams)
 	float fDiffIsAl, fDiffIsBe;
 	
 	fDiffIsAl = (ptIMstatObs->fIsAl - ptIMstatObs->fPrevIsAl)/ptIMparams->fDt;
-							ptIMstatObs->fPrevIsAl = ptIMstatObs->fIsAl;
+			ptIMstatObs->fPrevIsAl = ptIMstatObs->fIsAl;
 	
 	fDiffIsBe = (ptIMstatObs->fIsBe - ptIMstatObs->fPrevIsBe)/ptIMparams->fDt;
-							ptIMstatObs->fPrevIsBe = ptIMstatObs->fIsBe;
+			ptIMstatObs->fPrevIsBe = ptIMstatObs->fIsBe;
 	
 	ptIMstatObs->fEsAl = (ptIMstatObs->fUsAl - ptIMparams->fRs*ptIMstatObs->fIsAl - 
-											ptIMparams->fSigLs*fDiffIsAl)*ptIMparams->f1divKr;
+				ptIMparams->fSigLs*fDiffIsAl)*ptIMparams->f1divKr;
 						
 	ptIMstatObs->fEsBe = (ptIMstatObs->fUsBe - ptIMparams->fRs*ptIMstatObs->fIsBe -
-											ptIMparams->fSigLs*fDiffIsBe)*ptIMparams->f1divKr;
+				ptIMparams->fSigLs*fDiffIsBe)*ptIMparams->f1divKr;
 }
 
 /**
   * @brief  IM rotor back-EMF and flux observer calculation function
   * @param  ptIMrotObs: pointer to user data structure with type "tIMrotObs",
-  *			ptIMparams: pointer to user data structure with type "tIMparams".               
+  *	    ptIMparams: pointer to user data structure with type "tIMparams".               
   * @retval None
   */
 void tIMrotObs_calc(tIMrotObs* ptIMrotObs, tIMparams* ptIMparams)
 {
 	ptIMrotObs->fErAl = (ptIMrotObs->fIsAl*ptIMparams->fLm - ptIMrotObs->fFrAl)*
-											ptIMparams->f1divTr - ptIMrotObs->fWrE*ptIMrotObs->fFrBe;
+				ptIMparams->f1divTr - ptIMrotObs->fWrE*ptIMrotObs->fFrBe;
 	
 	ptIMrotObs->fFrAl = ptIMrotObs->fPrevFrAl + 0.5f*ptIMparams->fDt*(
-										 ptIMrotObs->fErAl + ptIMrotObs->fPrevErAl);
+				ptIMrotObs->fErAl + ptIMrotObs->fPrevErAl);
 	ptIMrotObs->fPrevErAl = ptIMrotObs->fErAl;
 	ptIMrotObs->fPrevFrAl = ptIMrotObs->fFrAl;
 						
 	ptIMrotObs->fErBe = (ptIMrotObs->fIsBe*ptIMparams->fLm - ptIMrotObs->fFrBe)*
-											ptIMparams->f1divTr + ptIMrotObs->fWrE*ptIMrotObs->fFrAl;
+				ptIMparams->f1divTr + ptIMrotObs->fWrE*ptIMrotObs->fFrAl;
 
 	ptIMrotObs->fFrBe = ptIMrotObs->fPrevFrBe + 0.5f*ptIMparams->fDt*(
-										 ptIMrotObs->fErBe + ptIMrotObs->fPrevErBe);
+				ptIMrotObs->fErBe + ptIMrotObs->fPrevErBe);
 	ptIMrotObs->fPrevErBe = ptIMrotObs->fErBe;
 	ptIMrotObs->fPrevFrBe = ptIMrotObs->fFrBe;
 }
@@ -110,7 +110,7 @@ void tIMrotObs_calc(tIMrotObs* ptIMrotObs, tIMparams* ptIMparams)
 /**
   * @brief  IM rotor speed and flux observer calculation function
   * @param  ptIMspeedObs: pointer to user data structure with type "tIMspeedObs",
-  *			ptIMparams: pointer to user data structure with type "tIMparams".               
+  *	    ptIMparams: pointer to user data structure with type "tIMparams".               
   * @retval None
   */
 void tIMspeedObs_calc(tIMspeedObs* ptIMspeedObs, tIMparams* ptIMparams)
@@ -129,19 +129,19 @@ void tIMspeedObs_calc(tIMspeedObs* ptIMspeedObs, tIMparams* ptIMparams)
 	
 	
 	ptIMspeedObs->sPI.fIn = ptIMspeedObs->fIsAl*(ptIMspeedObs->sIMstatObs.fEsBe - 
-												 ptIMspeedObs->sIMrotObs.fErBe) - ptIMspeedObs->fIsBe*(
-												 ptIMspeedObs->sIMstatObs.fEsAl - 
-												 ptIMspeedObs->sIMrotObs.fErAl);
+				ptIMspeedObs->sIMrotObs.fErBe) - ptIMspeedObs->fIsBe*(
+				ptIMspeedObs->sIMstatObs.fEsAl - 
+				ptIMspeedObs->sIMrotObs.fErAl);
 	
 	ptIMspeedObs->sPI.m_calc(&ptIMspeedObs->sPI);
 	
 	ptIMspeedObs->fWrE = ptIMspeedObs->sPI.fOut;
 	
 	ptIMspeedObs->fFrAng = atan2f(ptIMspeedObs->sIMrotObs.fFrBe,
-															ptIMspeedObs->sIMrotObs.fFrAl);
+				ptIMspeedObs->sIMrotObs.fFrAl);
 									
 	ptIMspeedObs->fFrMagn = hypotf(ptIMspeedObs->sIMrotObs.fFrBe,
-																ptIMspeedObs->sIMrotObs.fFrAl);
+					ptIMspeedObs->sIMrotObs.fFrAl);
 }
 
 /*********************************** END OF FILE ***********************************/
